fe0f66a46fc8c51894bbd26b306b8b317f84d802
Bander0162 <bndrnwaf46@gmail.com>
2025-07-21 16:21:02 +0300

    Create trade_limits.py

---
+++ trade_limits.py
@@ -0,0 +1,22 @@
+# تحديد عدد الصفقات اليومية
+MAX_TRADES = 5  # الحد الأقصى من الصفقات
+trade_count = 0  # العداد
+
+def can_open_trade():
+    global trade_count
+    if trade_count < MAX_TRADES:
+        return True
+    else:
+        return False
+
+def increment_trade():
+    global trade_count
+    if can_open_trade():
+        trade_count += 1
+        return True
+    else:
+        return False
+
+def reset_trades():
+    global trade_count
+    trade_count = 0
